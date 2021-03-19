f1c73b3666a99cbff85d02155f05b2d388ccbc6e
ByungSoo Ye <ybs680318@gmail.com>
2021-03-19 20:31:35 +0900

    Update: 1차 푸시에 대한 수정사항 반영 - 트랜잭션 추가, 필요없는 코드 제거, 불필요한 response 메시지 제거

--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
 import json, re, bcrypt
 from json.decoder import JSONDecodeError
 
-from django.views import View 
-from django.http  import JsonResponse
+from django.views            import View 
+from django.http             import JsonResponse
+from django.db               import transaction
+from django.core.exceptions  import ValidationError
 
 from user.models import User, Address
 
@@ -11,6 +13,7 @@ from user.models import User, Address
 # 1. email과 같은 key 값이 2개 이상 들어올 때는??
 # 2. 이벤트 SNS, 메시지 수신 여부
 class SignUpView(View):
+    @transaction.atomic
     def post(self, request):    
         try:
             data = json.loads(request.body)
@@ -18,7 +21,6 @@ class SignUpView(View):
             # 회원 개인 정보
             email         = data["email"]
             password      = data["password"]
-            password2     = data["password2"]
             name          = data["name"]
             phone_number  = data["phone_number"]
             date_of_birth = data.get("date_of_birth") # null True
@@ -31,48 +33,46 @@ class SignUpView(View):
             # 이메일 정규식 검사
             regex_for_email = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 
+            
+            Unauthroized_response = JsonResponse({"message": "UNAUTHORIZED"}, status=401)
+
             if not email:
-                return JsonResponse({"message": "이메일을 입력해주세요."}, status=400)
+                return Unauthroized_response
 
             if not regex_for_email.match(email):
-                return JsonResponse({"message": "이메일 형식이 적절하지 않습니다."}, status=400)
+                return Unauthroized_response
             
             if User.objects.filter(email=email).exists():
-                return JsonResponse({"message": "이미 존재하는 이메일입니다."}, status=400)
+                return JsonResponse({"message": "Conflict"}, status=409)
             
             # 비밀번호 검사
             if not password:
-                return JsonResponse({"message": "비밀번호를 입력해주세요."}, status=400)
-
-            if password != password2:
-                return JsonResponse({"message": "비밀번호가 일치하지 않습니다."}, status=400)
+                return Unauthroized_response
 
             if len(password) < 10:
-                return JsonResponse({"message": "비밀번호 길이는 10자 이상으로 해주세요."}, status=400)
+                return Unauthroized_response
 
             # 비밀번호 암호화
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
             decoded_hashed_pw = hashed_password.decode('utf-8') 
             
             if not name:
-                return JsonResponse({"message": "이름을 입력해주세요."}, status=400)
+                return Unauthroized_response
             
             if not phone_number:
-                return JsonResponse({"message": "휴대폰 번호를 입력해주세요."}, status=400) 
+                return Unauthroized_response
             
             if not zip_code and not address:
-                return JsonResponse({"message": "주소를 입력해주세요."}, status=400) 
+                return Unauthroized_response
         
             # 전화 번호 정규식 찾아보기 
             # regex_for_phone_number = re.compile('\d{9}')
             
-            User.objects.create(email=email, 
-                                password=decoded_hashed_pw, 
-                                name=name, 
-                                phone_number=phone_number, 
-                                date_of_birth=date_of_birth)
-            
-            user = User.objects.get(email=email)
+            user = User.objects.create(email=email, 
+                                        password=decoded_hashed_pw, 
+                                        name=name, 
+                                        phone_number=phone_number, 
+                                        date_of_birth=date_of_birth)
 
             Address.objects.create(user=user,
                                    zip_code=zip_code, 
@@ -83,9 +83,11 @@ class SignUpView(View):
             return JsonResponse({"message": "Sign Up SUCCESS"}, status=200)
 
         except KeyError:
-            return JsonResponse({"message": "필요한 값이 누락되었거나 key값이 잘못 입력되었습니다."}, status=500)
+            return JsonResponse({"message": "KEY_ERROR"}, status=500)
 
         except JSONDecodeError:
-            return JsonResponse({"message": "아무 정보도 보내지 않았습니다."}, status=500)
+            return JsonResponse({"message": "JSONDecodeError"}, status=500)
 
+        except ValidationError:
+            return JsonResponse({"message": "INVALID_VALUE"}, status=400)
 
