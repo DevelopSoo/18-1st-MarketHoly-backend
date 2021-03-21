894ec0cccabce5d868a262dbbbe565fb798c36da
ByungSoo Ye <ybs680318@gmail.com>
2021-03-21 19:46:59 +0900

    product/views.py 컨벤션 수정

--- product/views.py
+++ product/views.py
@@ -1,5 +1,4 @@
 # 이 상품 어때요?
-
 import random
 
 from django.views import View
@@ -15,44 +14,45 @@ from .models import Category, SubCategory, Product, DiscountRate
 class RecommendView(View):
     def get(self, request):
         try:
-            categories = Category.objects.all()
+            categories        = Category.objects.all()
             random_categories = random.sample(list(categories), 8) # 랜덤으로 뽑을 개수. 나중에는 이 개수를 12개로 늘린다.
             
             # 랜덤 카테고리를 기준으로 랜덤 서브 카테고리를 1개씩 추출
             random_sub_category_list = []
+
             for category in random_categories:
-                sub_categories = category.subcategory_set.all()
+                sub_categories      = category.subcategory_set.all()
                 random_sub_category = random.choice(sub_categories)
+
                 random_sub_category_list.append(random_sub_category)
             
             # 랜덤 서브 카테고리 1개당 1개의 랜덤 상품을 추출 
             random_product_list = []
+
             for sub_category in random_sub_category_list:
-                items = sub_category.product_set.all()
+                items          = sub_category.product_set.all()
                 random_product = random.choice(items)
+
                 random_product_list.append(random_product)
             
-
 
             listgoods = []
             for product in random_product_list:
             # 할인율이 있는 경우 없는 경우 나누기
                 if product.discountrate_set.all():
                     discount_rate = product.discountrate_set.all()[0].discount_rate
-                    product_info = {
-                        "image": product.image_url,
-                        "name": product.name,
-                        "price": product.price,
+                    product_info  = {
+                        "image"        : product.image_url,
+                        "name"         : product.name,
+                        "price"        : product.price,
                         "discount_rate": discount_rate
                     }
                 else:
                     product_info = {
                         "image": product.image_url,
-                        "name": product.name,
+                        "name" : product.name,
                         "price": product.price
                     }
-
-                
 
                 listgoods.append(product_info)
             
