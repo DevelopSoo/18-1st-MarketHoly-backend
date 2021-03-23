27027fd1896567602e59a975099313614bc8af67
ByungSoo Ye <ybs680318@gmail.com>
2021-03-23 21:47:43 +0900

    Add: MD 추천 관련 뷰 생성

--- product/views.py
+++ product/views.py
@@ -8,28 +8,34 @@ from .models import *
 
 class MDRecommendView(View):
     def get(self, request):
-        limit = request.GET['limit']
-        offset = request.GET['offset']
-        category_id = offset/limit
+        limit       = request.GET['limit']
+        offset      = request.GET['offset']
+        category_id = int(offset)/int(limit)
 
         category = Category.objects.get(id=category_id)
 
-        sub_categories = category.subcategory_set.all()
+        sub_categories        = category.subcategory_set.all()
         random_sub_categories = random.sample(list(sub_categories), 3)
 
         product_list_by_category = []
         for sub_category in random_sub_categories:
             two_products = sub_category.product_set.all().order_by('-stock')[:2]
+            
             product_list_by_category = [
-                    {"name": product.name,
-                    "image": product.image_url,
+                    {
+                    "name": product.name,
+                    "image_url": product.image_url,
                     "price": product.price,
-                    "discount_rate": product.discountrate_set.all()[0].discount_rate}
+                    "discount_rate": product.discountrate_set.all()[0].discount_rate
+                    }
                     if DiscountRate.objects.filter(product=product).exists() 
                     else 
-                    {"name": product.name,
-                    "image": product.image_url,
+                    {
+                    "name": product.name,
+                    "image_url": product.image_url,
                     "price": product.price,
-                    "discount_rate": None}
+                    "discount_rate": None
+                    }
                     for product in two_products]
+
         return JsonResponse({"product_list_by_category": product_list_by_category}, status=200)
